f59c227f79ea4abbf66bca71d433ba0d84ad7fcd
AlexMalt001 <alexmalt001@btinternet.com>
2017-04-16 18:16:52 +0100

    made class 'dataProcess' a namespace. removed 'using namespace dataProcess' from main.cpp: not used.

--- src/serialProcess.h
+++ src/serialProcess.h
@@ -3,11 +3,10 @@
 
 namespace serialProcess {
     void sendVars(int th, int st);
-};
+}
 
-class dataProcess {
-  public:
+namespace dataProcess {
   int processVars(int input, int upper, int lower, int deadzone, int upperPWM, int lowerPWM);
-};
+}
 
 #endif
